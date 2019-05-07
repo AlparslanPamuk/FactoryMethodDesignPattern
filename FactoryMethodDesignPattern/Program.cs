0ed278b0bce129a47f56b209c85e5eaf325bf4f5
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-07 16:22:57 +0300

    First Commit
    Creating Factory

---
+++ FactoryMethodDesignPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    //Creating Factory
+    public class LoggerFactory:IloggerFactory
+    {
+        public Ilogger CreateLogger()
+    }
+
+    public interface IloggerFactory
+    {
+
+    }
+
+    public interface Ilogger
+    {
+
+    }
+}
